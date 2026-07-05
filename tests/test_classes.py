bce759afc728989fe1d97352b966b9f9cb899d94
Валентин Юсупов <sobaka.zabiyaka@gmail.cha>
2026-07-05 14:21:49 +0300

    add tests for access logic

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -55,7 +55,10 @@ def test_category_initialization(
     assert category.description == (
         "Смартфоны, как средство не только коммуникации"
     )
-    assert category.products == [product, second_product]
+    assert category.products == (
+        "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
+        "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n"
+    )
 
 
 def test_category_count(
@@ -104,6 +107,73 @@ def test_empty_category_products() -> None:
         products=[],
     )
 
-    assert category.products == []
+    assert category.products == ""
     assert Category.category_count == 1
     assert Category.product_count == 0
+
+
+def test_add_product(product: Product, second_product: Product) -> None:
+    """Тестирует добавление товара в категорию."""
+    category = Category(
+        name="Смартфоны",
+        description="Смартфоны, как средство коммуникации",
+        products=[product],
+    )
+
+    category.add_product(second_product)
+
+    assert category.products == (
+        "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
+        "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n"
+    )
+    assert Category.product_count == 2
+
+
+def test_new_product() -> None:
+    """Тестирует создание товара через класс-метод."""
+    product_data = {
+        "name": "Xiaomi Redmi Note 11",
+        "description": "1024GB, Синий",
+        "price": 31000.0,
+        "quantity": 14,
+    }
+
+    product = Product.new_product(product_data)
+
+    assert product.name == "Xiaomi Redmi Note 11"
+    assert product.description == "1024GB, Синий"
+    assert product.price == 31000.0
+    assert product.quantity == 14
+
+
+def test_price_setter_positive_value(product: Product) -> None:
+    """Тестирует установку положительной цены."""
+    product.price = 190000.0
+
+    assert product.price == 190000.0
+
+
+def test_price_setter_zero_value(
+    product: Product,
+    capsys: pytest.CaptureFixture[str],
+) -> None:
+    """Тестирует запрет установки нулевой цены."""
+    product.price = 0
+
+    captured = capsys.readouterr()
+
+    assert product.price == 180000.0
+    assert "Цена не должна быть нулевая или отрицательная" in captured.out
+
+
+def test_price_setter_negative_value(
+    product: Product,
+    capsys: pytest.CaptureFixture[str],
+) -> None:
+    """Тестирует запрет установки отрицательной цены."""
+    product.price = -100
+
+    captured = capsys.readouterr()
+
+    assert product.price == 180000.0
+    assert "Цена не должна быть нулевая или отрицательная" in captured.out
