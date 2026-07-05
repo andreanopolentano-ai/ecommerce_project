1c0aea8a4262e6a9094ebc3290a513b1867002cc
Валентин Юсупов <sobaka.zabiyaka@gmail.cha>
2026-07-05 14:21:44 +0300

    update product and category access logic

--- src/classes.py
+++ src/classes.py
@@ -3,7 +3,6 @@ class Product:
 
     name: str
     description: str
-    price: float
     quantity: int
 
     def __init__(
@@ -16,8 +15,32 @@ class Product:
         """Инициализирует объект товара."""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @property
+    def price(self) -> float:
+        """Возвращает цену товара."""
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float) -> None:
+        """Устанавливает новую цену товара."""
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+
+        self.__price = new_price
+
+    @classmethod
+    def new_product(cls, product: dict) -> "Product":
+        """Создает новый товар из словаря."""
+        return cls(
+            name=product["name"],
+            description=product["description"],
+            price=product["price"],
+            quantity=product["quantity"],
+        )
 
 
 class Category:
@@ -25,7 +48,6 @@ class Category:
 
     name: str
     description: str
-    products: list[Product]
 
     category_count: int = 0
     product_count: int = 0
@@ -39,7 +61,25 @@ class Category:
         """Инициализирует объект категории товаров."""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, product: Product) -> None:
+        """Добавляет товар в категорию."""
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self) -> str:
+        """Возвращает список товаров категории в виде строки."""
+        products_str = ""
+
+        for product in self.__products:
+            products_str += (
+                f"{product.name}, {product.price} руб. "
+                f"Остаток: {product.quantity} шт.\n"
+            )
+
+        return products_str
